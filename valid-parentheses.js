d462751f6454fac00f77d0a7b855efa9db71f4ef
laruja <58679555+laruja@users.noreply.github.com>
2020-09-30 17:07:22 +0800

    Create valid-parentheses.js

---
+++ valid-parentheses.js
@@ -0,0 +1,40 @@
+/**
+ * 给定一个只包括 '('，')'，'{'，'}'，'['，']' 的字符串，判断字符串是否有效。
+
+有效字符串需满足：
+
+左括号必须用相同类型的右括号闭合。
+左括号必须以正确的顺序闭合。
+注意空字符串可被认为是有效字符串。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/valid-parentheses} s 
+ */
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = (s) => {
+    if(typeof s !== 'string'){
+        throw new Error('input shuld be string!');
+    }
+    if(s.length%2===1){
+        return false;
+    }
+   const map = {
+       '{':'}',
+       '[':']',
+       '(':')'
+   }
+   let stack = [];
+   for (let i = 0; i < s.length; i++) {
+       if(map[s[i]]){
+           stack.push(s[i]);
+       }else{
+           if(s[i]!==map[stack.pop()]){
+               return false;
+           }
+       }
+   }
+   return stack.length === 0;
+}
